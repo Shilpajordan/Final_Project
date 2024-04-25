a413b4d73eab347b09ae4e038f7110bcaa1df1d1
jgohl-dci <johannes.gohl@dci-student.org>
2024-04-25 15:43:42 +0200

    add frontend, static files, base html, create urls

--- doc_search/views.py
+++ doc_search/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'doc_search/index.html')
+
+
+def login(request):
+    return render(request, 'doc_search/login.html')
