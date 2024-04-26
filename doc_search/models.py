9ff5d9331412e1841d2f76ed7614b76571158710
jgohl-dci <johannes.gohl@dci-student.org>
2024-04-26 14:12:21 +0200

    edit format models

--- doc_search/models.py
+++ doc_search/models.py
@@ -1,49 +1,46 @@
 from django.db import models
 
-# Create your models here.
 
 class Patient(models.Model):
-    CHOICES = [('headache','Headache'),
-               ('backpain','Backpain'),
-               ('kneepain','Kneepain'),
-               ('fever','Fever'),
-               ('cough','Cough'),
-               ('throat ache','Throat ache'),
-               ('sneezing','Sneezing'),
-               ('body pain','Body pain'),
-               ('tooth ache','Tooth ache'),
-                ]
-    first_name = models.CharField(max_length=50, null =False)
-    last_name = models.CharField(max_length=50, null =False)
-    age = models.IntegerField(null =False)
+    CHOICES = [
+        ('headache', 'Headache'),
+        ('backpain', 'Backpain'),
+        ('kneepain', 'Kneepain'),
+        ('fever', 'Fever'),
+        ('cough', 'Cough'),
+        ('throat ache', 'Throat ache'),
+        ('sneezing', 'Sneezing'),
+        ('body pain', 'Body pain'),
+        ('tooth ache', 'Tooth ache'),
+        ]
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    age = models.IntegerField(null=False)
     gender = models.CharField(max_length=10)
-    email = models.EmailField(null =False)
-    insurance = models.CharField(max_length=50, null =False)
-    complaint = models.CharField(max_length=50, choices=CHOICES, null =False)
-
+    email = models.EmailField(null=False)
+    insurance = models.CharField(max_length=50, null=False)
+    complaint = models.CharField(max_length=50, choices=CHOICES, null=False)
 
 
 class Doctor(models.Model):
-    SPEZ_CHOICES =[
-        ('GP','General Practitioner'),
-         ('IM','Internal Medicine'),
-          ('ORTH','Orthopedic'),
-          ('CARD','Cardiologist'),
-          ('DENT','Dentist'),
-          ('GYN','Gynecologist'),
-          ('PUL','Pulmonologist')
+    SPEZ_CHOICES = [
+        ('GP', 'General Practitioner'),
+        ('IM', 'Internal Medicine'),
+        ('ORTH', 'Orthopedic'),
+        ('CARD', 'Cardiologist'),
+        ('DENT', 'Dentist'),
+        ('GYN', 'Gynecologist'),
+        ('PUL', 'Pulmonologist')
     ]
-    first_name = models.CharField(max_length=50, null =False)
-    last_name = models.CharField(max_length=50, null =False)
-    location = models.CharField(max_length=50, null =False)
-    specialization = models.CharField(max_length=50, choices=SPEZ_CHOICES, null =False)
-    consultation_hours = models.CharField(max_length=50, null =False)
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    location = models.CharField(max_length=50, null=False)
+    specialization = models.CharField(max_length=50, choices=SPEZ_CHOICES, null=False)
+    consultation_hours = models.CharField(max_length=50, null=False)
 
 
 class Appointment(models.Model):
-
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     date = models.DateTimeField()
     time = models.TimeField()
-    
