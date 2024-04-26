e895715e7e2a4ef269bb50eb7cf561f39dad26d9
jgohl-dci <johannes.gohl@dci-student.org>
2024-04-26 11:45:01 +0200

    add login/logout

--- doc_search/views.py
+++ doc_search/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'doc_search/index.html')
 
 
-def login(request):
-    return render(request, 'doc_search/login.html')
+@login_required
+def ov_appoint(request):
+    return render(request, 'doc_search/ov_appoint.html')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('doc_search:ov_appoint')
+            else:
+                messages.error(request, 'Invalid username or password')
+
+    else:
+        form = LoginForm()
+    return render(request, 'doc_search/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('doc_search:login')
