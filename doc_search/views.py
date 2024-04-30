762abbdb772f2ab94de21ac015c746eef355fe27
jgohl-dci <johannes.gohl@dci-student.org>
2024-04-30 16:07:30 +0200

    edit oppointment overview

--- doc_search/views.py
+++ doc_search/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Doctor
+from .models import Doctor, Appointment
 from .forms import LoginForm
 
 
@@ -13,7 +13,8 @@ def home(request):
 @login_required
 def ov_appoint(request):
     doctors = Doctor.objects.all()
-    return render(request, 'doc_search/ov_appoint.html', {"doctors": doctors})
+    appointments = Appointment.objects.all()
+    return render(request, 'doc_search/ov_appoint.html', {"doctors": doctors, "appointments": appointments})
 
 
 def user_login(request):
