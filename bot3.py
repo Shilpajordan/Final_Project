b5d6d11c684765e96df7b4d65f27d18c4dedf42b
Andi <andreas.rajchert@dci-student.org>
2024-05-07 10:45:25 +0200

    chatbot will search for all docs in db with show doctors

--- bot3.py
+++ bot3.py
@@ -1,6 +1,7 @@
 from difflib import get_close_matches
 import json
 import random
+import sqlite3
 
 def load_knowledge_base(file_path: str) -> dict:
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -15,18 +16,39 @@ def find_best_match(user_question: str, intents: list[str]) -> str | None:
     return best_match[1] if best_match[0] else None
 
 def get_answer_for_question(question: str, json_answer: dict) -> str | None:
-    default_response = "I don't know"
     for intent in json_answer["Intents"]:
         if question in intent["patterns"]:
             return random.choice(intent["responses"])
     
-    return default_response
-            
+
+def get_all_doctors():
+    try:
+        
+        conn = sqlite3.connect('db.sqlite3')
+        cursor = conn.cursor()
+
+        
+        query = "SELECT * FROM doc_search_doctor"
+        cursor.execute(query)
+        
+        
+        doctors = cursor.fetchall()
+
+        
+        conn.close()
+
+        return doctors
+      
+    except sqlite3.Error as error:
+        print("Error querying SQLite database:", error)
+        return None
+
 
 def chat_bot():
     knowledge_base: dict = load_knowledge_base("intents.json")
     intents = {intent['tag']: intent['patterns'] for intent in knowledge_base['Intents']}
     #saving tag and patterns as a dictionary
+
 
     while True:
         user_input: str = input("You: ").lower()
@@ -34,13 +56,27 @@ def chat_bot():
         if user_input == "quit":
             break
 
+        
         best_match: str | None = find_best_match(user_input, intents)
 
         if best_match:
             answer: str = get_answer_for_question(best_match, knowledge_base)
             print(f"Bot: {answer}")
+
+        elif user_input.lower() == "show doctors":
+            all_doctors = get_all_doctors()
+
+            if all_doctors:
+                print("Bot: Here are all the doctors:")
+                for doctor in all_doctors:
+                    print(doctor)  
+
+            else:
+                print("Bot: There are no doctors in the database.")
+        
         else:
             print(f"Bot: I don't know.")
+
 
 if __name__ == "__main__":
     chat_bot()
