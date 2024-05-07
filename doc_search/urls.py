1f63633ed5b6a56ae30cd53202f8673d96be4019
jgohl-dci <johannes.gohl@dci-student.org>
2024-05-07 15:30:28 +0200

    currently broken, dont pull

--- doc_search/urls.py
+++ doc_search/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.user_login, name="login"),
     path('logout/', views.user_logout, name='user_logout'),
-    path("ov_appoint/", views.ov_appoint, name="ov_appoint"),
+    path("ov_appoint/<str:doc_id>/", views.ov_appoint, name="ov_appoint"),
     path("detail_appoint/<int:patient_id>/", views.detail_appoint, name="detail_appoint"),
 ]
