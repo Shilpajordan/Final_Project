47a9b77f6fd6347026bef833da39eafd984779ca
Ajil Abdalqader <49165584+ciwwan1980@users.noreply.github.com>
2024-05-15 15:25:22 +0200

    Update models.py
    adding extra model to my model-and just keep everything related to book appointment as my file

--- doc_search/models.py
+++ doc_search/models.py
@@ -33,14 +33,47 @@ class Doctor(models.Model):
         ('GYN', 'Gynecologist'),
         ('PUL', 'Pulmonologist')
     ]
+     FLOOR_CHOICES = [
+        ('F1', 'Floor 1'),
+        ('F2', 'Floor 2'),
+        ('F3', 'Floor 3'),
+        ('F4', 'Floor 4')
+    ]
+    ROOM_CHOICES = [
+        ('R1', 'Room 1'),
+        ('R2', 'Room 2'),
+        ('R3', 'Room 3'),
+        ('R4', 'Room 4')
+    ]
     first_name = models.CharField(max_length=50, null =False)
     last_name = models.CharField(max_length=50, null =False)
     location = models.CharField(max_length=50, null =False)
     specialization = models.CharField(max_length=50, choices=SPEZ_CHOICES, null =False)
     consultation_hours = models.CharField(max_length=50, null =False)
-    
+    floor_location = models.CharField(max_length=50, choices=FLOOR_CHOICES, null=False, default="Floor 1")
+    room_location = models.CharField(max_length=50, choices=ROOM_CHOICES, null=False, default="Room 1")
+
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+class BusinessHours(models.Model):
+    DAY_CHOICES = [
+        ('Mon', 'Monday'),
+        ('Tue', 'Tuesday'),
+        ('Wed', 'Wednesday'),
+        ('Thu', 'Thursday'),
+        ('Fri', 'Friday'),
+        ('Sat', 'Saturday'),
+    ]
+    weekday = models.CharField(max_length=50, choices=DAY_CHOICES, null=False)
+    start_hour = models.TimeField()
+    end_hour = models.TimeField()
+
+    class Meta:
+        ordering = ['id',]
+        verbose_name = "Business Hour"
+        verbose_name_plural = "Business Hours"
 
 class Appointment(models.Model):
     patient_id = models.CharField(max_length=50)
