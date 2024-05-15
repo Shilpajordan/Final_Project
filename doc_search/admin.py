c5bb7f2bb0c02e74982089810cc19c393e93a991
Ajil <ajilqado@gmail.com>
2024-05-15 12:12:56 +0200

    rendering-booking-appointment to another page in base.html- delete booking appointment section in index.html-adding-design-css to new-page-no changes in model

--- doc_search/admin.py
+++ doc_search/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from doc_search.models import Patient, Doctor,  Appointment
+from doc_search.models import Patient, Doctor,  Appointment, TimeSlot
 
 
 admin.site.register(Patient)
 admin.site.register(Doctor)
 admin.site.register(Appointment)
+admin.site.register(TimeSlot)
--- doc_search/forms.py
+++ doc_search/forms.py
@@ -17,19 +17,36 @@ SPEZ_CHOICES = [
     ('GYN', 'Gynecologist'),
     ('PUL', 'Pulmonologist')
 ]
+GENDERS = [
+    ('','Please select your gender'),
+    ('MALE','Male'),
+    ('FEMALE','Female')
+]
 class AppointmentBookingForm(forms.ModelForm):
     specialization = forms.ChoiceField(choices=SPEZ_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
-    doctor = forms.ModelChoiceField(queryset=Doctor.objects.all(), empty_label="Please select a doctor")
+    doctor = forms.ModelChoiceField(queryset=Doctor.objects.all(), empty_label="Please select a doctor", widget=forms.Select(attrs={'class': 'form-control'}))
     date = forms.ChoiceField(choices=[], widget=forms.Select(attrs={'class': 'form-control'}))
     patient_firstname = forms.CharField(max_length=100) 
     patient_lastname = forms.CharField(max_length=100)  
     patient_age = forms.CharField(max_length=100) 
-    patient_gender = forms.CharField(max_length=100) 
+    patient_gender = forms.ChoiceField(choices=GENDERS, widget=forms.Select(attrs={'class': 'form-control'}))
     patient_email = forms.CharField(max_length=100)  
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['date'].choices = self.get_time_slot_choices()
+
+        # Set placeholders for each field
+        self.fields['specialization'].widget.attrs['placeholder'] = 'Please select a specialization'
+        self.fields['doctor'].widget.attrs['placeholder'] = 'Please select a doctor'
+        self.fields['date'].widget.attrs['placeholder'] = 'Please select a date'
+        self.fields['patient_firstname'].widget.attrs['placeholder'] = 'Enter first name'
+        self.fields['patient_lastname'].widget.attrs['placeholder'] = 'Enter last name'
+        self.fields['patient_age'].widget.attrs['placeholder'] = 'Enter age'
+        self.fields['patient_gender'].widget.attrs['placeholder'] = 'Enter gender'
+        self.fields['patient_email'].widget.attrs['placeholder'] = 'Enter email'
+
+
  
 
     def get_time_slot_choices(self):
