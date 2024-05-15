73b3ac086bf699d7451a32049be97bfb67cb2b5b
Andi <andreas.rajchert@dci-student.org>
2024-05-15 13:53:29 +0200

    feedback is working

--- bot/chatbot_logic.py
+++ bot/chatbot_logic.py
@@ -3,6 +3,8 @@ import random
 import sqlite3
 from difflib import get_close_matches
 
+waiting_for_feedback = False
+
 
 def load_knowledge_base(file_path: str) -> dict:
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -35,22 +37,34 @@ def get_all_doctors():
 def collect_feedback(feedback):
     with open("feedback.txt", "a") as file:
         file.write(feedback + "\n")
+    global waiting_for_feedback
+    waiting_for_feedback = False
+    return "Thank you for your feedback!"
 
 def chat_bot(user_input):
-    knowledge_base: dict = load_knowledge_base("intents.json")
-    intents = {intent['tag']: intent['patterns'] for intent in knowledge_base['Intents']}
-    best_match = find_best_match(user_input, intents)
-    if best_match:
-        return get_answer_for_question(best_match, knowledge_base)
-    elif user_input.lower() == "show doctors":
-        all_doctors = get_all_doctors()
-        if all_doctors:
-            return "Here are all the doctors: " + ", ".join(str(doctor) for doctor in all_doctors)
-        else:
-            return "There are no doctors in the database."
-    elif user_input.lower() == "give feedback":
-        feedback = input("Now please write your feedback: ")
-        collect_feedback(feedback)
-        return ({'bot_response': 'Vielen Dank für Ihr Feedback!'})
+    global waiting_for_feedback
+
+    
+    user_input = user_input.strip().lower()  
+    
+    if waiting_for_feedback:
+        return collect_feedback(user_input)  # If waiting for feedback, treat the input as feedback
+    elif user_input == "give feedback":
+        waiting_for_feedback = True
+        return "Please write your feedback:"
+    
+
     else:
-        return "I don't know. If you want information about our doctors, write 'show doctors'. If you want to give us feedback, write 'give feedback'."
+        knowledge_base: dict = load_knowledge_base("intents.json")
+        intents = {intent['tag']: intent['patterns'] for intent in knowledge_base['Intents']}
+        best_match = find_best_match(user_input, intents)
+        if best_match:
+            return get_answer_for_question(best_match, knowledge_base)
+        elif user_input.lower() == "show doctors":
+            all_doctors = get_all_doctors()
+            if all_doctors:
+                return "Here are all the doctors: " + ", ".join(str(doctor) for doctor in all_doctors)
+            else:
+                return "There are no doctors in the database."
+        else:
+            return "I don't know. If you want information about our doctors, write 'show doctors'. If you want to give us feedback, write 'give feedback'."
