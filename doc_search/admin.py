451167334b254949f8e0bf9b28d4becef5b8e15b
Ajil Abdalqader <49165584+ciwwan1980@users.noreply.github.com>
2024-05-15 14:45:35 +0200

    Update admin.py

--- doc_search/admin.py
+++ doc_search/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from doc_search.models import Patient, Doctor,  Appointment, TimeSlot
+from doc_search.models import Patient, Doctor, BusinessHours, Appointment, TimeSlot
 
 
 admin.site.register(Patient)
 admin.site.register(Doctor)
 admin.site.register(Appointment)
 admin.site.register(TimeSlot)
+@admin.register(BusinessHours)
+class BusinessHoursAdmin(admin.ModelAdmin):
+    list_display = ('weekday', 'start_hour', 'end_hour')
