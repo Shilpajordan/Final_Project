66a0efc7c6ff1c39c5a369f545876adf97cb16c8
jgohl-dci <johannes.gohl@dci-student.org>
2024-05-22 15:22:21 +0200

    test for home and login

--- doc_search/tests.py
+++ doc_search/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ResponseTest(TestCase):
+
+    def test_call_view_home(self):
+        '''
+        Testing url response
+        '''
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'doc_search/index.html')
+
+
+class LoginTestCase(TestCase):
+    '''
+    Testing login and response of @login_required url
+    '''
+    def setUp(self):
+        self.client = Client()
+        # setting up a user in our empty test database
+        self.user = User.objects.create_user('admin', 'xxx@xxx.com', '1234')
+
+    def test_call_view_ov_appoint(self):
+        self.client.login(username='admin', password='1234')
+        response = self.client.get('/ov_appoint/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'doc_search/ov_appoint.html')
